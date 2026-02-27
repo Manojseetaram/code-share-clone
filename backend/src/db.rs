3e028640de0cc98825efd8aebd18ddac64c4dc46
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-28 00:15:10 +0530

    conert the var

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -6,7 +6,7 @@ use mongodb::{
 use tracing::info;
 
 pub async fn get_database() -> Database {
-    let mongo_url = std::env::var("MONGODB_URL")
+    let mongo_url = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "mongodb+srv://manojseetaramdev_db_user:i1jm3zhrdVYoEQZk@cluster0.alofyrd.mongodb.net/".into());
 
     let mut opts = ClientOptions::parse(&mongo_url)
